7103414cd73949a1757c50e4b249515e83752f58
Alexandros Kritikos <akritikos@outlook.com>
2023-09-26 15:34:01 +0300

    🚚 Sanitizes method names

--- src/Spectre.Infrastructure/CommandBuilder.cs
+++ src/Spectre.Infrastructure/CommandBuilder.cs
@@ -1,15 +1,15 @@
-﻿namespace Kritikos.CommandLine.Hosting;
+﻿namespace Kritikos.Spectre.Infrastructure;
+
+using global::Spectre.Console.Cli;
 
 using Microsoft.Extensions.DependencyInjection;
-
-using Spectre.Console.Cli;
 
 /// <summary>
 /// A builder for <see cref="ICommandApp"/> instances.
 /// </summary>
 public sealed class CommandBuilder
 {
-  public CommandBuilder(IServiceCollection? services = null, ITypeRegistrar? registrar = null)
+  private CommandBuilder(IServiceCollection? services = null, ITypeRegistrar? registrar = null)
   {
     Services = services ?? new ServiceCollection();
     Registrar = registrar ?? new TypeRegistrar(Services);
@@ -24,16 +24,23 @@ public sealed class CommandBuilder
   /// <summary>
   /// Creates a new <see cref="ICommandApp"/> instance with default options.
   /// </summary>
-  /// <param name="services">An implementation of a service provider for dependency injection.</param>
-  /// <param name="registrar">The type registrar to be used.</param>
   /// <typeparam name="T">The <see cref="ICommandAppStartup"/> to use both for registering <see cref="CommandSettings"/> and configuring the resulting <see cref="ICommandApp"/>.</typeparam>
   /// <returns>A properly configured <see cref="ICommandApp"/> with dependency injection.</returns>
-  public static ICommandApp CreateDefaultCommandApp<T>(
+  public static ICommandApp CreateDefaultCommandApp<T>()
+    where T : ICommandAppStartup
+    => new CommandBuilder()
+      .UseStartup<T>()
+      .RegisterSettingsFromAssemblyContaining<T>()
+      .Build();
+
+  /// <summary>
+  /// Creates a new <see cref="CommandBuilder"/> allowing further customization before building.
+  /// </summary>
+  /// <param name="services">An implementation of a service provider for dependency injection.</param>
+  /// <param name="registrar">The type registrar to be used.</param>
+  /// <returns>A <see cref="CommandBuilder"/> instance.</returns>
+  public static CommandBuilder CreateDefaultBuilder(
     IServiceCollection? services = null,
     ITypeRegistrar? registrar = null)
-    where T : ICommandAppStartup
-    => new CommandBuilder(services, registrar)
-      .UseStartup<T>()
-      .RegisterSettingsFromAssembly<T>()
-      .Build();
+    => new(services, registrar);
 }
--- src/Spectre.Infrastructure/CommandBuilderExtensions.cs
+++ src/Spectre.Infrastructure/CommandBuilderExtensions.cs
@@ -1,15 +1,16 @@
-﻿namespace Kritikos.CommandLine.Hosting;
+﻿namespace Kritikos.Spectre.Infrastructure;
 
 using System.Collections.Immutable;
 using System.Globalization;
+using System.Reflection;
 
-using Kritikos.CommandLine.Logging;
+using global::Spectre.Console.Cli;
+
+using Kritikos.Spectre.Logging;
 
 using Microsoft.Extensions.DependencyInjection;
 
 using Serilog;
-
-using Spectre.Console.Cli;
 
 public static class CommandBuilderExtensions
 {
@@ -35,10 +36,22 @@ public static class CommandBuilderExtensions
   /// <param name="builder">The instance of <see cref="CommandBuilder"/> to configure.</param>
   /// <typeparam name="T">The <see cref="ICommandAppStartup"/> to load <see cref="CommandSettings"/> from.</typeparam>
   /// <returns>The configured <paramref name="builder"/>.</returns>
-  public static CommandBuilder RegisterSettingsFromAssembly<T>(this CommandBuilder builder)
+  public static CommandBuilder RegisterSettingsFromAssemblyContaining<T>(this CommandBuilder builder)
   {
     ArgumentNullException.ThrowIfNull(builder);
     var assembly = typeof(T).Assembly;
+
+    return builder.RegisterSettingsFromAssembly(assembly);
+  }
+
+  /// <summary>
+  /// Registers all &lt;see cref="CommandSettings"/&gt; implementations found in the <paramref name="assembly"/>.
+  /// </summary>
+  /// <param name="builder">The instance of <see cref="CommandBuilder"/> to configure.</param>
+  /// <param name="assembly">The <see cref="Assembly"/> containing wanted <see cref="CommandSettings"/>.</param>
+  /// <returns>The configured <paramref name="builder"/>.</returns>
+  public static CommandBuilder RegisterSettingsFromAssembly(this CommandBuilder builder, Assembly assembly)
+  {
     var settings = assembly
       .GetTypes()
       .Where(x => typeof(CommandSettings).IsAssignableFrom(x))
@@ -52,7 +65,12 @@ public static class CommandBuilderExtensions
     return builder;
   }
 
-  public static CommandBuilder AddLogging(this CommandBuilder builder)
+  /// <summary>
+  /// Adds logging to the <see cref="ICommandApp"/> instance.
+  /// </summary>
+  /// <param name="builder">The instance of <see cref="CommandBuilder"/> to configure.</param>
+  /// <returns>The configured <paramref name="builder"/>.</returns>
+  public static CommandBuilder AddFileLogging(this CommandBuilder builder)
   {
     ArgumentNullException.ThrowIfNull(builder);
 
