331c3b82ca43a9fddf2c4f04b46bb1b6b8c76b2e
Alexandros Kritikos <akritikos@outlook.com>
2023-10-15 21:11:51 +0300

    🚸 Adds support for cancellation tokens on commands

---
+++ src/SpectreCli.Infrastructure/CancellableAsyncCommand.cs
@@ -0,0 +1,53 @@
+﻿#pragma warning disable SA1402
+namespace Kritikos.SpectreCli.Infrastructure;
+
+using Spectre.Console.Cli;
+
+/// <summary>
+/// Base class for an asynchronous command with cancellation support.
+/// </summary>
+/// <typeparam name="TCommandSettings">The settings type.</typeparam>
+public abstract class CancellableAsyncCommand<TCommandSettings> : AsyncCommand<TCommandSettings>
+  where TCommandSettings : CommandSettings
+{
+  /// <summary>
+  /// Executes the command.
+  /// </summary>
+  /// <param name="context">The command context.</param>
+  /// <param name="settings">The settings.</param>
+  /// <param name="cancellationToken">A <see cref="CancellationToken"/> to observe while waiting for the task to complete.</param>
+  /// <returns>An integer indicating whether or not the command executed successfully.</returns>
+  public abstract Task<int> ExecuteAsync(
+    CommandContext context,
+    TCommandSettings settings,
+    CancellationToken cancellationToken);
+
+  /// <inheritdoc />
+  public override async Task<int> ExecuteAsync(CommandContext context, TCommandSettings settings)
+  {
+    var cancellationSource = new PosixCancellationTokenSource();
+    var cancellable = ExecuteAsync(context, settings, cancellationSource.Token);
+    return await cancellable;
+  }
+}
+
+/// <summary>
+/// Base class for an asynchronous command with cancellation support.
+/// </summary>
+public abstract class CancellableAsyncCommand : CancellableAsyncCommand<EmptyCommandSettings>
+{
+  /// <summary>
+  /// Executes the command.
+  /// </summary>
+  /// <param name="context">The command context.</param>
+  /// <param name="cancellationToken">A <see cref="CancellationToken"/> to observe while waiting for the task to complete.</param>
+  /// <returns>An integer indicating whether or not the command executed successfully.</returns>
+  public abstract Task<int> ExecuteAsync(CommandContext context, CancellationToken cancellationToken);
+
+  /// <inheritdoc />
+  public override Task<int> ExecuteAsync(
+    CommandContext context,
+    EmptyCommandSettings settings,
+    CancellationToken cancellationToken)
+    => ExecuteAsync(context, cancellationToken);
+}
---
+++ src/SpectreCli.Infrastructure/PosixCancellationTokenSource.cs
@@ -0,0 +1,41 @@
+﻿namespace Kritikos.SpectreCli.Infrastructure;
+
+using System.Runtime.InteropServices;
+
+/// <summary>
+/// A POSIX-compatible handler of cancellation logic for console apps
+/// </summary>
+internal sealed class PosixCancellationTokenSource : IDisposable
+{
+  private readonly CancellationTokenSource cancellationSource = new();
+  private readonly List<PosixSignalRegistration> signalRegistrations = new();
+
+  public PosixCancellationTokenSource()
+  {
+    var sigInt = PosixSignalRegistration.Create(PosixSignal.SIGINT, OnSignal);
+    var sigQuit = PosixSignalRegistration.Create(PosixSignal.SIGQUIT, OnSignal);
+    var sigTerm = PosixSignalRegistration.Create(PosixSignal.SIGTERM, OnSignal);
+
+    signalRegistrations.Add(sigInt);
+    signalRegistrations.Add(sigQuit);
+    signalRegistrations.Add(sigTerm);
+  }
+
+  public CancellationToken Token
+    => cancellationSource.Token;
+
+  /// <inheritdoc />
+  public void Dispose()
+  {
+    foreach (var signal in signalRegistrations)
+    {
+      signal.Dispose();
+    }
+  }
+
+  private void OnSignal(PosixSignalContext ctx)
+  {
+    ctx.Cancel = true;
+    cancellationSource.Cancel();
+  }
+}
