82f9cb6745fc28f7d2da7d63b0791dd1a5a6e6b4
Alexandros Kritikos <akritikos@outlook.com>
2025-08-16 16:31:11 +0300

    ♻️ Uses HostBuilder injection to facilitate deferred service instantiation

--- src/SpectreCli.Hosting/CommandBuilder.cs
+++ src/SpectreCli.Hosting/CommandBuilder.cs
@@ -2,29 +2,26 @@ namespace Kritikos.SpectreCli.Hosting;
 
 using Kritikos.SpectreCli.Hosting.HostBuilderDependencies;
 
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 using Spectre.Console.Cli;
 
 public sealed class CommandBuilder
 {
-  public static ICommandApp CreateHostedCommandApp<T>(params string[] args)
+  /// <summary>
+  /// Builds a <see cref="CommandApp"/> using the provided <typeparamref name="T"/> as the startup configuration.
+  /// </summary>
+  /// <typeparam name="T"></typeparam>
+  /// <returns></returns>
+  public static ICommandApp CreateHostedCommandApp<T>()
     where T : ICommandAppServiceInjector, ICommandAppConfiguration
   {
     var host = Host.CreateDefaultBuilder()
-      .UseConsoleLifetime()
       .ConfigureServices(T.ConfigureServices);
 
     var registrar = new GenericHostBuilderTypeRegistrar(host);
     var app = new CommandApp(registrar);
     app.Configure(T.Configure);
-
-    host.ConfigureServices(services =>
-    {
-      services.AddSingleton(new CommandAppWrapper() { App = app, Args = args });
-      services.AddHostedService<CommandAppService>();
-    });
 
     return app;
   }
